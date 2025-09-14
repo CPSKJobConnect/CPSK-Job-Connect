dd120f8768e4d02e254e1a88de2ad0d865d4da67
Thanchida <thanchida.a@ku.th>
2025-09-14 21:47:39 +0700

    chore: add mock data

--- src/app/(student)/jobs/page.tsx
+++ src/app/(student)/jobs/page.tsx
@@ -6,6 +6,8 @@ import JobDescriptionCard from "@/components/JobDescriptionCard";
 import { filterJobs } from "@/lib/jobFilter";
 import { JobInfo } from "@/types/job";
 import { JobFilterInfo } from "@/types/filter";
+import { fakeJobData } from "public/data/fakeJobDescription";
+import { fakeFilterInfo } from "public/data/fakeFilterInfo";
 import { FaRegFileAlt } from "react-icons/fa";
 import { MdTipsAndUpdates } from "react-icons/md";
 import { IoMdSearch } from "react-icons/io";
@@ -20,7 +22,8 @@ export default function Page() {
 
   useEffect(() => {
     // fetch job data
-
+    setFilterInfo(fakeFilterInfo);
+    setJobData(fakeJobData);
   }, []);
 
   useEffect(() => {
