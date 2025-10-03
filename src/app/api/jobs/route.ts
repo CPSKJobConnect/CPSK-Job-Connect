77b18ebb2b524c6c8637ba47e051d30c81e9fc3e
NapatrSaengthongsakullert <napatr.se@ku.th>
2025-10-04 02:01:38 +0700

    feat: make jobs can access to necessary fields in the jobPost model

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -5,34 +5,45 @@ export async function GET() {
   try {
     const jobs = await prisma.jobPost.findMany({
       include: {
-        types: true,
-        arrangements: true,
         categories: true,
         tags: true,
+        applications: true,
+        company: {
+          include: {
+            account: true,
+          },
+        },
+        jobType: true,
+        jobArrangement: true,
       },
     });
 
+
     const mappedData = jobs.map((job) => ({
       id: job.id,
+      companyLogo: job.company.account?.logoUrl ?? "",
+      companyBg: job.company.account?.backgroundUrl ?? "",
       jobName: job.jobName,
+      companyName: job.company.name,
+      category: job.categories.map((c) => c.name),
+      location: job.location,
+      posted: job.created_at.toISOString(),
+      applied: job.applications.length,
+      minSalary: job.min_salary.toString(),
+      maxSalary: job.max_salary.toString(),
+      type: job.jobType.name,
       description: {
         aboutRole: job.aboutRole,
         requirements: job.requirements,
         qualifications: job.qualifications,
       },
-      minSalary: job.min_salary.toString(),
-      maxSalary: job.max_salary.toString(),
-      posted: job.created_at,
-      location: job.location,
-      type: job.types.map((t) => t.name),
-      arrangement: job.arrangements.map((a) => a.name),
-      category: job.categories.map((c) => c.name),
       tags: job.tags.map((tag) => tag.name),
+      arrangement: job.jobArrangement.name,
     }));
-
+    console.log("Mapped jobs to JSON:", mappedData);
     return NextResponse.json(mappedData);
   } catch (error) {
-    console.error(error);
+    console.error("API error:", error);
     return NextResponse.json({ error: "Failed to fetch jobs" }, { status: 500 });
   }
 }
