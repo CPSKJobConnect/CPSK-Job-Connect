2339c6ae853a86886474cdc15a4d2d02cff0c737
Thanchida <thanchida.a@ku.th>
2025-10-17 14:12:14 +0700

    ui: Implement Application Status chart

--- src/app/student/dashboard/StatusPieChart.tsx
+++ src/app/student/dashboard/StatusPieChart.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import React from "react";
 import { PieChart } from "@mantine/charts";
-import { Card, Title } from "@mantine/core";
+import { Card, Title, Group, Box, Text } from "@mantine/core";
 import { mockStatusPie } from "@public/data/mockStudentStat";
 import { useState, useEffect } from "react";
 
@@ -13,11 +14,12 @@ interface StatusPieProps {
 export default function StatusPieChart() {
   const [data, setData] = useState<StatusPieProps[]>([]);
   const statusColors: Record<string, string> = {
-    Pending: "#FFDEDE",
-    Offered: "#FF8551",
-    Interview: "#ADE4DB",
-    Rejected: "#FAF0E4",
+    Pending: "#FFF4CC",
+    Offered: "#C8E6C9",
+    Interview: "#B3E5FC",
+    Rejected: "#F8D7DA",
   };
+
 
   useEffect(() => {
     setData(mockStatusPie.map(item => ({
@@ -27,24 +29,34 @@ export default function StatusPieChart() {
   }, [])
 
   return (
-    <Card shadow="md" radius="lg" p="lg" className="w-full">
+    <Card shadow="md" radius="lg" p="md" className="w-full">
       <Title order={4} mb="md" className="text-gray-800 text-center">
         Application Status Breakdown
       </Title>
-      <div className="flex justify-center items-center">
+
+      <div className="relative flex justify-center items-center">
         <PieChart
           data={data.map(item => ({
             ...item,
             color: statusColors[item.name] || "gray.4",
           }))}
           withTooltip
+          tooltipDataSource="segment"
           withLabels
           labelsPosition="outside"
-          size={250}
+          size={230}
           strokeWidth={1}
-          tooltipDataSource="segment"
           strokeColor="white"
         />
+
+        <div className="absolute bottom-2 right-1 border border-gray-100 shadow-md p-2 flex flex-col gap-1 bg-white rounded">
+          {data.map(item => (
+            <Group key={item.name}>
+              <Box w={14} h={14} style={{ backgroundColor: statusColors[item.name] }} />
+              <Text size="sm">{item.name}</Text>
+            </Group>
+          ))}
+        </div>
       </div>
     </Card>
   );
