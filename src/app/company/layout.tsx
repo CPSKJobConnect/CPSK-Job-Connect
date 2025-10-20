7101cd86b8c830d2c926ef555e56678222ccf2fe
gemme_38 <gemme_38@hotmail.com>
2025-10-20 12:47:41 +0700

    fix weird bg colors

--- src/app/company/layout.tsx
+++ src/app/company/layout.tsx
@@ -1,18 +1,18 @@
-// "use client";
+"use client";
 
-// import CompanyNavbar from "@/components/Navbar/CompanyNavbar";
+import CompanyNavbar from "@/components/Navbar/CompanyNavbar";
 
-// type Props = {
-//   children: React.ReactNode;
-// };
+type Props = {
+  children: React.ReactNode;
+};
 
-// export default function StudentLayout({ children }: Props) {
-//   return (
-//     <div className="min-h-screen flex flex-col">
-//       <div className="fixed top-0 left-0 w-full z-50">
-//         <CompanyNavbar />
-//       </div>
-//       <main className="flex-1 p-4 mt-16">{children}</main>
-//     </div>
-//   );
-// }
+export default function StudentLayout({ children }: Props) {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#F2FFFB]">
+      <div className="fixed top-0 left-0 w-full z-50">
+        <CompanyNavbar />
+      </div>
+      <main className="flex-1 p-4 mt-16">{children}</main>
+    </div>
+  );
+}
