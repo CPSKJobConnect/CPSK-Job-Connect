92c597ea92341be1e617616e1ba724fd364a77f4
gemme_38 <gemme_38@hotmail.com>
2025-10-21 22:15:23 +0700

    Admin approve-company page

--- src/app/admin/approve-company/page.tsx
+++ src/app/admin/approve-company/page.tsx
@@ -17,7 +17,7 @@ import {
   DialogDescription,
   DialogFooter
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
+// import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import {
