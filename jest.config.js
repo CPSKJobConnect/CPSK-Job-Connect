336c57afbb6d71235a9b96dd5efdb56e6d92c679
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2025-11-15 22:00:23 +0700

    feat: make the dependency compatible with window os

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jest-environment-node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transform: {
     '^.+\\.(t|j)sx?$': ['ts-jest'],
@@ -12,4 +12,41 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/'
+  ],
+  projects: [
+    {
+      displayName: 'components',
+      testEnvironment: 'jest-environment-jsdom',
+      testMatch: ['<rootDir>/src/components/**/*.test.{ts,tsx}'],
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+      transform: {
+        '^.+\\.(t|j)sx?$': ['ts-jest'],
+      },
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+    },
+    {
+      displayName: 'api',
+      testEnvironment: 'jest-environment-node',
+      testMatch: ['<rootDir>/src/tests/**/*.test.{ts,tsx}', '<rootDir>/src/app/api/**/*.test.{ts,tsx}'],
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+      transform: {
+        '^.+\\.(t|j)sx?$': ['ts-jest'],
+      },
+      transformIgnorePatterns: [
+        '/node_modules/(?!jose|openid-client|next-auth|@panva/hkdf|uuid|preact|preact-render-to-string)',
+      ],
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+    },
+  ],
 };
